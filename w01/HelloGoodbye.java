39ef32b78dcd7e01fd95ebbe5d59703ea9d578ce
Thayakorn Rakwetpakorn <man.gamerdot@hotmail.com>
2020-07-25 04:34:11 +0900

    Add HelloGoodbye.java

---
+++ w01/HelloGoodbye.java
@@ -0,0 +1,8 @@
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        String p1 = args[0];
+        String p2 = args[1];
+        System.out.printf("Hello %s and %s.\n", p1, p2);
+        System.out.printf("Hello %s and %s.", p2, p1);
+    }
+}
