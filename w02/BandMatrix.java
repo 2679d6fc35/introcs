8f416eab62563b0a0d68065dc6a2ee8f1fc33fd6
syncrodazt <man.gamerdot@hotmail.com>
2020-08-03 17:48:14 +0900

    Fix BandMatrix.java and GeneralizedHarmonic.java

--- w02/BandMatrix.java
+++ w02/BandMatrix.java
@@ -4,11 +4,10 @@ public class BandMatrix {
         int width = Integer.parseInt(args[1]);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (Math.abs(i-j)<=width)
-                {
-                    System.out.printf("* ");
+                if (Math.abs(i - j) <= width) {
+                    System.out.printf("*  ");
                 } else {
-                    System.out.printf("0 ");
+                    System.out.printf("0  ");
                 }
             }
             System.out.printf("\n");
